f8296dbc853d6660ce0ee631ac9eeb1844ed640a
Eric Schles <erschles@microsoft.com>
2018-07-19 20:14:19 -0400

    adding publish method and test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     return "Hello world"
+
+@app.route("/publish", methods=["GET", "POST"])
+def publish():
+    posting = request.form["posting"]
+    return posting
+
 
 if __name__ == '__main__':
     app.run()
--- tests.py
+++ tests.py
@@ -7,3 +7,6 @@ def test_index():
     assert requests.get("http://localhost:5000/").text == requests.get("http://localhost:5000/index").text
     assert requests.get("http://localhost:5000/").text.lower() == "hello world" 
     
+def test_publish():
+    assert requests.get("http://localhost:5000/publish", data={"posting": "Hello there"}).status_code == 200
+    assert requests.get("http://localhost:5000/publish", data={"posting": "Hello there"}).text == "Hello there"
